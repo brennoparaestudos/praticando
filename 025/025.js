1cea1809d9f0408074b42c543ff1e88ee157c6b9
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-05-08 18:04:11 -0300

    praticando

--- 025/025.js
+++ 025/025.js
@@ -2,10 +2,12 @@ let compras = []
 
 compras.push('arroz', 'feijão', 'carne', 'frango', 'bisteca')
 
+ compras.sort()
+
 console.log(`Essa é a quantidade de itens que tem na lista ${compras.length}`)
 
 for(let indice = 0; indice < compras.length; indice++){
-    compras.sort()
 
     console.log(`${compras[indice]}`)
+
 }
