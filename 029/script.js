2d2dea98d70f25b00afe0b7e5fb4e80f9015db65
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-05-30 16:36:08 -0300

    objeto

---
+++ 029/script.js
@@ -0,0 +1,20 @@
+const jogador = {
+    nome: 'Brenno',
+    time: 'Corinthians',
+    posicao: 'Volante',
+    numero_da_camisa: 21,
+    gols_marcados_na_temp: 7
+}
+
+
+
+console.log(jogador.nome);
+console.log(jogador.time);
+console.log(jogador.posicao);
+console.log(jogador.numero_da_camisa);
+console.log(jogador.gols_marcados_na_temp);
+
+
+jogador.cartoes = 2
+
+console.log(jogador)
