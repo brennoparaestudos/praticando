fe210810f2883f42c6259138394df83b10f41785
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-06-16 15:10:28 -0300

    falta terminar ex 1

---
+++ 033/ex1/script.js
@@ -0,0 +1,18 @@
+let btnAdd = document.getElementById("btn-add")
+let ul = document.getElementById("lista")  
+let btnRmv = document.getElementById("btn-rmv")
+
+  btnAdd.addEventListener('click', function() {
+    let texto = document.getElementById('itexto').value
+    let li = document.createElement("li")
+
+    li.appendChild(document.createTextNode(texto))
+    ul.appendChild(li)
+
+  })
+
+
+  btnRmv.addEventListener("click", function(){
+    
+  })
+  
