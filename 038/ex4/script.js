c77032a66b1edf4d427c4649d1e141a34ebefb36
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-08-08 15:21:25 -0300

    praticando exs de logica

---
+++ 038/ex4/script.js
@@ -0,0 +1,11 @@
+function maiorDeTres(a, b, c ) {
+    if(a >= b && a >= c){
+        return a
+    } else if(b >= a && b >= c){
+        return b
+    } else{
+        return c
+    }
+}
+
+console.log(maiorDeTres(21, 17, 11))
---
+++ 038/ex5/script.js
@@ -0,0 +1,6 @@
+function contagemRegressiva(n){
+    for(let i = n; i >= 1 ; i--){
+        console.log(i)
+    }
+}
+contagemRegressiva(5)
